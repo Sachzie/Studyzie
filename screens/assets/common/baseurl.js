d50e91b953610b57b428fc1aa3fcf3fbd943ceb3
intronerd12 <bumatayjilia@gmail.com>
2026-03-03 16:38:23 +0800

    Point mobile API to Hugging Face Space

--- screens/assets/common/baseurl.js
+++ screens/assets/common/baseurl.js
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native'
 
-let baseURL = 'https://3b22-2405-8d40-4058-aabd-bc0a-de9d-e35c-a77.ngrok-free.app/api/v1/'
+let baseURL = 'https://ruin13-studyzie.hf.space/api/v1/'
 
 export default baseURL;
